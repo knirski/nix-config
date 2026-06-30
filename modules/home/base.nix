347dfa54e85133631d69c843fb3b15f4df3e366b
knirski <krzysztof.nirski+github@gmail.com>
2026-06-30 23:55:02 +0200

    chore: add direnv config (.envrc, nix-direnv in Home Manager)
    Co-authored-by: CommandCodeBot <noreply@commandcode.ai>

--- modules/home/base.nix
+++ modules/home/base.nix
@@ -9,6 +9,11 @@
       programs.git.enable = true;
       programs.home-manager.enable = true;
 
+      programs.direnv = {
+        enable = true;
+        nix-direnv.enable = true;
+      };
+
       home.packages = with pkgs; [
         fd
         ripgrep
