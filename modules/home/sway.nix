0c829a9ac990147c8d9e9a64f0578ad26e39c331
Krzysztof Nirski <krzysztof.nirski+github@gmail.com>
2026-07-15 02:01:48 +0200

    Revert "fix(sway): float swappy via extraConfig for_window rule"
    This reverts commit bbef9eb2be64c8741738406f56c27adf6233475a.

--- modules/home/sway.nix
+++ modules/home/sway.nix
@@ -12,7 +12,6 @@
         extraConfig = ''
           output eDP-1 scale 1
           output DP-6 scale 1
-          for_window [app_id="swappy"] floating enable
         '';
         config = rec {
           modifier = "Mod4";
@@ -28,6 +27,7 @@
           # remains compositor/application-owned for middle-click pasting.
           startup = [ ];
           bars = [ ];
+          for_window."app_id == \"swappy\"" = "floating enable";
           keybindings = {
             "${modifier}+Return" = "exec ${terminal}";
             "${modifier}+Q" = "kill";
