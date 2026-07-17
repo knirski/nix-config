37e281c99f25fd0a29484516e144aaf0fecd128c
Krzysztof Nirski <krzysztof.nirski+github@gmail.com>
2026-07-17 06:33:43 +0200

    fixup: set explicit owner/group/mode for persisted dms-greeter dir

--- hosts/zbook/persistence.nix
+++ hosts/zbook/persistence.nix
@@ -23,7 +23,12 @@ in
       # Without this, the key vanishes on reboot and unattended backups fail.
       "/etc/restic"
       "/var/log"
-      "/var/lib/dms-greeter"
+      {
+        directory = "/var/lib/dms-greeter";
+        user = "greeter";
+        group = "greeter";
+        mode = "0750";
+      }
     ];
     files = [
       {
