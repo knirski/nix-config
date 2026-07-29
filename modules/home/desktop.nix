db8cb755f7d92d89b15554fc746e83eba2d9bc22
Krzysztof Nirski <krzysztof.nirski+github@gmail.com>
2026-07-29 22:28:38 +0200

    fix: add pinentry wrapper delegating to pinentry-gnome3

--- modules/home/desktop.nix
+++ modules/home/desktop.nix
@@ -13,10 +13,6 @@
           # (the env var is simply unused).
           lib.mkIf pkgs.stdenv.isLinux {
             GTK_THEME = "Adwaita:dark";
-            # rbw spawns pinentry directly (not through gpg-agent) to read the
-            # master password. Point it at the GTK3 pinentry we already have
-            # configured for gpg-agent below.
-            PINENTRY = "${pkgs.pinentry-gnome3}/bin/pinentry-gnome3";
           };
 
         # Neovim clipboard integration (requires wl-clipboard on Wayland)
@@ -37,6 +33,14 @@
           ++ lib.optionals stdenv.isLinux [
             bitwarden-desktop
             rbw
+            # rbw spawns `pinentry` directly (not through gpg-agent). The
+            # nixpkgs `pinentry` meta-package was removed; provide a thin
+            # wrapper that delegates to the GTK3 pinentry we already have.
+            (pkgs.writeShellApplication {
+              name = "pinentry";
+              runtimeInputs = [ pkgs.pinentry-gnome3 ];
+              text = ''exec pinentry-gnome3 "$@"'';
+            })
             wl-clipboard
             loupe
             freetube
